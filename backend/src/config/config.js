bf46c29dfa5452e1dc06a006c4742cf024cd664e
Subham777-max <subhamray865@gmail.com>
2026-05-17 10:51:59 +0530

    feat(config): update configuration management and remove deprecated app.config.js

---
+++ backend/src/config/config.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is not defined in environment variables");
+}
+export const config = {
+    port: process.env.PORT || 3000,
+    aiServiceUrl: process.env.AI_SERVICE_URL || 'http://localhost:8000',
+    MONGO_URI: process.env.MONGO_URI,
+    CORS_ORIGIN: process.env.CORS_ORIGIN || "http://localhost:5173",
+    MAX_FILE_SIZE: process.env.MAX_FILE_SIZE || 10 * 1024 * 1024, // 10MB default
+    UPLOAD_DIR: process.env.UPLOAD_DIR || 'uploads',
+}
